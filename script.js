1444081184a7f87094e72c2cdfb2e4e70d7ccd75
Eishalex2 <t.whitney.a@gmail.com>
2022-12-04 19:36:53 -0800

    feat: Have display show clicked buttons/operators
    Wired up the number and operator buttons to populate the display. Next
    challenge is going to be figuring out how to store the numbers and
    operators that are clicked. Thinking an array, with num1 and num2 in
    spots 0 and 2, and the operator in spot 1. But then how do you tell it
    to move on to the next array spot?

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const numBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const display = document.getElementById('display');
+const decimal = document.getElementById('decimal');
+
 function add(a, b) {
   return a + b;
 }
@@ -33,3 +38,25 @@ function operate(operator, a, b) {
       break;
   }
 }
+
+numBtns.forEach(num => {
+  num.addEventListener('click', () => {
+    if (display.textContent.includes('+' || '-' || 'x' || '÷')) {
+      display.textContent = '';
+    };
+    display.textContent += num.textContent;
+  });
+});
+
+operatorBtns.forEach(operator => {
+  operator.addEventListener('click', () => {
+    display.textContent = operator.textContent;
+  })
+})
+
+//store the first number that's input, the operator, and then the second
+//number that's input. Then, when you click the equals button, run the
+//operate function on the array and display the output.
+//only focusing on 1 set for now.
+
+
